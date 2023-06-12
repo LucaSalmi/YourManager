bfc9206531a78c815afddf519040b043b22721bf
lucaSalmi <luca.salmi90@gmail.com>
2023-06-12 11:41:36 +0200

    Initial Commit

---
+++ YourManager/YourManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourManagerApp.swift
+//  YourManager
+//
+//  Created by Luca Salmi on 2023-06-12.
+//
+
+import SwiftUI
+
+@main
+struct YourManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
